f711e9cdbad3e7a6f237e9019e03520ebfa6e031
Omar Alaa <omar98alaa@gmail.com>
2025-07-17 16:43:41 +0300

    Added Favourite repository and service.

--- Eskon.Service/ServiceDependencyInjection.cs
+++ Eskon.Service/ServiceDependencyInjection.cs
@@ -13,6 +13,7 @@ namespace Eskon.Service
             services.AddTransient<IUserService, UserService>();
             services.AddTransient<IBookingService, BookingService>();
             services.AddTransient<IAuthenticationService, AuthenticationService>();
+            services.AddTransient<IFavouriteService, FavouriteService>();
 
             //Authentication
             var jwtSettings = new JwtSettings();
