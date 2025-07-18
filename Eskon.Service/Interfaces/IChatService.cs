1e4f4a2802e3b14a1ff5b42f317378f948892fff
Abdelrahman-Mostafa2025 <abdelrahmanashour817@gmail.com>
2025-07-18 06:27:46 +0300

    Add for chat &chat messages repos & services

---
+++ Eskon.Service/Interfaces/IChatService.cs
@@ -0,0 +1,17 @@
+﻿using Eskon.Domian.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eskon.Service.Interfaces
+{
+    interface IChatService
+    {
+        Task<Chat> AddChatAsync(Chat chat);
+        Task<Chat?> GetChatByIdAsync(Guid chatId);
+        Task<List<Chat>> GetUserChatsAsync(Guid userId);
+        Task<bool> ChatExistsAsync(Guid user1Id, Guid user2Id);
+    }
+}
---
+++ Eskon.Service/Services/ChatMessagesService.cs
@@ -0,0 +1,37 @@
+﻿using Eskon.Domian.Models;
+using Eskon.Infrastructure.Interfaces;
+using Eskon.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eskon.Service.Services
+{
+    public class ChatMessageService : IChatMessagesService
+    {
+        private readonly IChatMessageRepository _chatMessageRepository;
+
+        public ChatMessageService(IChatMessageRepository chatMessageRepository)
+        {
+            _chatMessageRepository = chatMessageRepository;
+        }
+
+        public async Task<ChatMessage> AddMessageAsync(ChatMessage chatMessage)
+        {
+            return  await _chatMessageRepository.AddAsync(chatMessage);
+        }
+
+        public async Task<List<ChatMessage>> GetMessagesAsync(Guid chatId)
+        {
+           return await _chatMessageRepository.GetFilteredAsync(c=>c.ChatId==chatId);
+        }
+
+        public async Task<ChatMessage?> GetMessageByIdAsync(Guid messageId)
+        {
+           return await _chatMessageRepository.GetByIdAsync(messageId);
+        }
+    }
+
+}
