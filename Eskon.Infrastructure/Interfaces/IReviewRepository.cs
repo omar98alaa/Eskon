c44f27facf82639a353da8929a37f320413172c4
Abdelrahman Mostafa <abdelrahmanashour817@gmail.com>
2025-07-18 15:30:56 +0300

    Update and rename IReviewReposatory.cs to IReviewRepository.cs

--- Eskon.Infrastructure/Interfaces/IReviewRepository.cs
+++ Eskon.Infrastructure/Interfaces/IReviewRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Eskon.Infrastructure.Interfaces
 {
-    public interface IReviewReposatory : IGenericRepositoryAsync<Review>
+    public interface IReviewRepository : IGenericRepositoryAsync<Review>
     {
     
     }
