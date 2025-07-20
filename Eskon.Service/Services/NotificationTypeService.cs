bfe3c4752dda2a8ec8faff5668e524fb0684dedf
John-Emad <john_emad7@outlook.com>
2025-07-20 15:17:15 +0300

    - Adding NotificationType repository and interface. - Adding NotificationType service and interface.

---
+++ Eskon.Service/Services/NotificationTypeService.cs
@@ -0,0 +1,73 @@
+﻿
+using Eskon.Domian.Models;
+using Eskon.Infrastructure.Interfaces;
+using Eskon.Service.Interfaces;
+
+namespace Eskon.Service.Services
+{
+    public class NotificationTypeService : INotificationTypeService
+    {
+        #region Fields
+        private readonly INotificationTypeRepository _notificationTypeRepository;
+        #endregion
+
+        #region Constructors
+        public NotificationTypeService(INotificationTypeRepository notificationTypeRepository)
+        {
+            _notificationTypeRepository = notificationTypeRepository;
+        }
+        #endregion
+
+        #region Methods
+
+        #region Create
+        public async Task<NotificationType> AddNotificationTypeAsync(NotificationType notificationType)
+        {
+            return await _notificationTypeRepository.AddAsync(notificationType);
+        }
+        #endregion
+
+        #region Read
+        public async Task<NotificationType> GetNotificationTypeByIdAsync(Guid id)
+        {
+            return await _notificationTypeRepository.GetByIdAsync(id);
+        }
+
+        public async Task<NotificationType> GetNotificationTypeByNameAsync(string name)
+        {
+            return await _notificationTypeRepository.GetNotificationTypeByNameAsync(name);
+        }
+
+        public async Task<List<NotificationType>> GetAllNotificationTypesAsync()
+        {
+            return await _notificationTypeRepository.GetAllAsync();
+        }
+        #endregion
+
+        #region Update
+        public async Task EditNotificationTypeAsync(NotificationType notificationType)
+        {
+            notificationType.UpdatedAt = DateTime.UtcNow;
+            await _notificationTypeRepository.UpdateAsync(notificationType);
+        }
+        #endregion
+
+        #region Delete
+        public async Task SoftDeleteNotificationTypeAsync(NotificationType notificationType)
+        {
+            await _notificationTypeRepository.SoftDeleteAsync(notificationType);
+        }
+
+        public async Task DeleteNotificationTypeAsync(NotificationType notificationType)
+        {
+            await _notificationTypeRepository.DeleteAsync(notificationType);
+        } 
+        #endregion
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _notificationTypeRepository.SaveChangesAsync();
+        }
+        #endregion
+    }
+}
