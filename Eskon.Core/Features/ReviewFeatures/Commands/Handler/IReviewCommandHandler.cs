749cedc46ae713c6142bdf0887d7f64061e712bd
Omar Alaa <omar98alaa@gmail.com>
2025-08-06 17:49:46 +0300

    Added Review commands and queries

---
+++ Eskon.Core/Features/ReviewFeatures/Commands/Handler/IReviewCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using Eskon.Core.Features.ReviewFeatures.Commands.Command;
+using Eskon.Core.Response;
+using Eskon.Domian.DTOs.ReviewDTOs;
+using MediatR;
+
+namespace Eskon.Core.Features.ReviewFeatures.Commands.Handler
+{
+    public interface IReviewCommandHandler : IRequestHandler<AddReviewCommand, Response<ReviewReadDTO>>,
+                                             IRequestHandler<EditReviewCommand, Response<ReviewReadDTO>>,
+                                             IRequestHandler<DeleteReviewCommand, Response<ReviewReadDTO>>;
+}
