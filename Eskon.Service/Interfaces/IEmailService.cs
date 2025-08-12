4d285d23cdf52d0fb7c8bf15e3bc1aae5ad23a1f
Omar Alaa <omar98alaa@gmail.com>
2025-08-12 23:38:34 +0300

    Added FluentEmail config and service.

---
+++ Eskon.Service/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Eskon.Service.Interfaces
+{
+    public interface IEmailService
+    {
+        public Task SendEmailAsync(string To, string Subject, string Body);
+    }
+}
---
+++ Eskon.Service/Services/EmailService.cs
@@ -0,0 +1,19 @@
+﻿using Eskon.Service.Interfaces;
+using FluentEmail.Core;
+
+namespace Eskon.Service.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IFluentEmail _fluentEmail;
+
+        public EmailService(IFluentEmail fluentEmail)
+        {
+            _fluentEmail = fluentEmail;
+        }
+        public async Task SendEmailAsync(string To, string Subject, string Body)
+        {
+            await _fluentEmail.To(To).Subject(Subject).Body(Body).SendAsync();
+        }
+    }
+}
